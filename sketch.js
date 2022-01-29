daa4051f0f931657095d83d68dc43b13f3945170
TigmanshNatani <88618744+TigmanshNatani@users.noreply.github.com>
2022-01-29 01:06:05 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,62 +1,128 @@
- var universe2Img,universe2,universebg1,universebg1Img,Asteroid1,Asteroid1Img;
- var RocketImg,Rocket,leftboundary,rightboundary;
- 
+ var Mbackground,Mobstacle,marios,Mcloud1,upboundary,bottomboundary,reset,coin;
+ var MbackgroundImg,MobstacleImg,marioImg,mario1Img,mario_running,Mcloud1Img,resetImg,coinImg;
+ var score = 0;
+
  function preload(){
-  universe2Img = loadImage("universe 2.png");
-  universebg1Img = loadAnimation("universebg-1.png")
-  RocketImg = loadImage("Rocket.png");
-  Asteroid1Img = loadImage("Asteroid1.png");
+  MbackgroundImg = loadImage("M.background.png");
+  MobstacleImg = loadImage("M.obstacle.png");
+  mario_running = loadImage("mario1.png","mario.png");
+  Mcloud1Img = loadImage("M.cloud1.png");
+  resetImg = loadImage("reset button.png");
+  coinImg = loadImage("coin.png");
 }
 function setup() {
-    createCanvas(400,400);
-    universe2 = createSprite(200,200);
-    universe2.addImage("universe 2",universe2Img);
-    universe2.y = universe2.width /2;
-    universe2.velocityY = 5;
-    
-    Rocket = createSprite(200,350);
-    Rocket.addImage("Rocket",RocketImg);
-    Rocket.scale = 0.1;
+    createCanvas(400,600);
 
-    leftboundary = createSprite(35,0,50,800);
-    leftboundary.visible = false;
+    Mbackground = createSprite(400,300);
+    Mbackground.addImage(MbackgroundImg);
+    Mbackground.scale = 0.3;
 
-    rightboundary = createSprite(365,0,50,800);
-    rightboundary.visible = false;
+    Mobstacle = createSprite(350,435);
+    Mobstacle.addImage(MobstacleImg);
+    Mobstacle.scale = 0.13;
 
+    marios = createSprite(100,405,10,10);
+    marios.addImage(mario_running);
+    marios.scale = 0.2;
+  
+    reset = createSprite(200,125,5,5);
+    reset.addImage(resetImg);
+    reset.scale = 0.05;
+     
+      score = 0;
  }
 
 function draw() {
-  if(keyDown(RIGHT_ARROW)){
-    Rocket.velocityX += 5;
-  }
-    if(keyDown(LEFT_ARROW)){
-      Rocket.velocityX -= 5;
-  }
-  var select_Asteroid = Math.round(random(1,4));
-  if(World.frameCount % 100 == 0){
-    switch(select_Asteroid){
-      case 1: Asteroid1();
-      break;
-      default:break;
-    }
-  }
-  
-  Rocket.collide(leftboundary);
-  Rocket.collide(rightboundary);
+  background("white");
+  textSize(25);
+  fill("green");
+  stroke("yellow");
+  text("score:"+score,50,50);
 
-  if(universe2.y > 400){
-     universe2.y = height/2;
+
+  Mobstacle.setCollider("circle",0,0,100); 
+
+  if(keyDown(UP_ARROW)){
+    marios.velocityY = -15;
+  }
+  if(marios.y < 250){
+     marios.velocityY = 15;
+   }
+  if(marios.y > 405){
+     marios.velocityY = 0;
+   }
+  if(keyDown(UP_ARROW)){
+   if(marios.y > 405){
+     marios.velocityY = -15;
+   }
+  }
+  if(keyDown("space")){
+    Mbackground.velocityX = -15;
+    Mobstacle.velocityX = -15;
+   // Mcloud1.velocityX = -5;
+   // coin.velocityX = -10;                  
   }
   
-  Asteroid1()
-  drawSprites()
+  // if mario waill tought the obstacle, then what will happen?
+  if(marios.isTouching(Mobstacle)){
+   Mbackground.velocityX = -0;
+   Mobstacle.velocityX = -0;
+   Mcloud1.velocityX = -0;
+   marios.velocityY = -0;
+   coin.velocityX = -0;
+   textSize(19);
+   stroke("red");
+   text("OH! You Lost " ,200,50);
+  }
+
+  if(Mbackground.x < 0){
+     Mbackground.x = width/4;
+  }
+  if(Mobstacle.x < 0){
+    Mobstacle.x = width/1;
+  }
+ /* if(Mcloud1.x < 0){
+    Mcloud1.x = width/2;
+
+  }*/
+  if(marios.x < 0){
+    marios.x = width/2;
+  }
+ /* if(coin.x < 0){
+    coin.x = width/2;
+  }*/
+
+ /* if(marios.isTouching(coin)){
+    score = score+1;
+  }*/
+  
+  
+  spawnCoins();
+ spawnClouds();
+  drawSprites();
 }
-function Asteroid1(){
-  var Asteroid = createSprite(0,Math.round(random(20,370)),10,10);
-  Asteroid.addImage(Asteroid1Img);
-  Asteroid.velocityY = 1;
-  Asteroid.lifetime = 100;
-  Asteroid.scale = 0.1;
 
+//function to spawn the clouds
+function spawnClouds(){
+  if(frameCount%100===0){
+    // write your code here 
+ Mcloud1=createSprite(375,600,10,10);
+ Mcloud1.y = random(100,275);
+ Mcloud1.velocityX=-5;
+ Mcloud1.addImage(Mcloud1Img);
+ Mcloud1.depth = marios.depth;
+ marios.depth=marios.depth+1;
+  }
+}
+function spawnCoins(){
+  if(frameCount%50===0){
+    coin = createSprite(400,600,10,10);
+    coin.y = random(200,475);
+    coin.velocityX = -10;
+    coin.addImage(coinImg);
+    coin.setCollider("circle",0,0,100);
+    coin.scale = 0.3;
+    coin.depth = marios.depth;
+    marios.depth = marios.depth+1;   
+  }
 }
